c289284b6777d218ea4ad36819fae0f5359a4e3d
liubao68 <bismy@qq.com>
2023-10-10 14:40:43 +0800

    publish website

--- content/references/java-chassis/zh_CN/search/main.js
+++
@@ -1,5 +0,0 @@
-$(".wy-side-nav-search").append('<div role="search"> \
-<form id ="rtd-search-form" class="wy-form" action="https://cn.bing.com"> \
-<input type="text" name="q" placeholder="搜索..." value=""/> \
-<input type="hidden" name="q1" value="site:' + window.location.host + '"/> \
-  </form></div>')
